e25037ccf950cc0d32e28595b9b2ec0375591865
Jason Volney <54520187+JasonVolney@users.noreply.github.com>
2022-09-05 17:12:21 -0300

    feat: knex

--- src/main/configs/knex.ts
+++ src/main/configs/knex.ts
@@ -1,13 +1,13 @@
-import Knex from "Knex"
+import Knex from "Knex";
 
 const knex = Knex({
-    client: "mysql2",
-    connection: {
-        host: "localhost",
-        user: "root",
-        database: "library",
-        password: "bcd127"
-    }
-})
+  client: "mysql2",
+  connection: {
+    host: "localhost",
+    user: "root",
+    database: "library",
+    password: "bcd127",
+  },
+});
 
-export { knex }
+export { knex };
